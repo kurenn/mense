07a3e72de3b3d30387e39a11422f357107d6a437
Abraham Kuri <kurenn@icalialabs.com>
2021-11-24 00:12:26 +0000

    Adds config for mense client api key

--- spec/mense_spec.rb
+++ spec/mense_spec.rb
@@ -1,11 +1,17 @@
 # frozen_string_literal: true
 
 RSpec.describe Mense do
-  it "has a version number" do
-    expect(Mense::VERSION).not_to be nil
+  it { is_expected.to respond_to(:api_key) }
+
+  it 'returns the set value for the api key' do
+    Mense.configure do |config|
+      config.api_key = 'a-sample-api-key'
+    end
+
+    expect(Mense.api_key).to eql 'a-sample-api-key'
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "has a version number" do
+    expect(Mense::VERSION).not_to be nil
   end
 end
